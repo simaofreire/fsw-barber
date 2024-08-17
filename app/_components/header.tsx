3a00797d2732f8be175f87d145e6a878bf7eacba
Simão Freire <simaofreire@live.com>
2024-08-17 16:38:07 -0300

    feat: Add header component and UI card component

---
+++ app/_components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+export const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/Logo.png" height={18} width={120} alt="Logo" priority />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import { Header } from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Click</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Hello, Simão</h2>
+        <p>Segunda-feira, 01 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/BannerPizza.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Banner pizza"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
